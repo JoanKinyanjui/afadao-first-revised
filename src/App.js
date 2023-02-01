b23bad2ff9123de2b9f09dcc1b4a631cdaa7d9cc
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2023-02-01 19:55:49 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
+// eslint-disable-next-line
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import Enroll from './Components/Enroll/Enroll';
 
 function App() {
+
   return (
+    <>
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+    <Route exact path='/' element={<Home/>} />
+    <Route exact path='/enroll' element={<Enroll />} />
+    </Routes>
+      </div>
+    </Router>
+    </>
   );
 }
 
